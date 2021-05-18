d43c236533158e537aa6b55e5d8879e422a156dd
Ivan Murataya <mura.ivan@hotmail.com>
2021-05-18 11:34:32 -0700

    Initial Commit

---
+++ ListCounties/ListCountiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListCountiesApp.swift
+//  ListCounties
+//
+//  Created by user188298 on 5/18/21.
+//
+
+import SwiftUI
+
+@main
+struct ListCountiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
